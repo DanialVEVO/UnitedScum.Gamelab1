382ee1feb1ecdd5c99f28e94583f8069839cc991
Chievap <robert98neijmeijer@gmail.com>
2015-10-13 11:17:57 +0200

    Rocks final

---
+++ Unity/Robert/Robert/Assets/Script/Rocks/RockDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockDestroy : MonoBehaviour {
+
+	public float timer = 5;
+
+
+	void Awake () {
+		StartCoroutine(Timer());
+	}
+	
+	IEnumerator Timer(){
+		yield return new WaitForSeconds(timer);
+		Object.Destroy(gameObject);		
+	}
+
+}
